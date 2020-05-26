4770b8e941df0c5835e3301f6ecd1dc87031cd16
Matheus Cardoso <matheus@cardo.so>
2020-05-26 20:56:58 -0300

    Update README.md

--- Smitch/Smitch/AppDelegate.swift
+++ Smitch/Smitch/AppDelegate.swift
@@ -15,11 +15,11 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        VoxeetSDK.shared.initialize(consumerKey: "ZTBib3I3NzkzMmt0aA==", consumerSecret: "NDUyM2kzMTc0ZHNvZWxjaHRucG41dmpidnE=")
+        VoxeetSDK.shared.initialize(consumerKey: "ZTBib3I5IaNzmmt0aA==", consumerSecret: "NDUyM2kzjaHRucNvZWxjaHRucG41dmpidnE=")
         VoxeetSDK.shared.appGroup = "group.so.cardo.Smitch.Group"
         VoxeetSDK.shared.preferredExtension = "so.cardo.Smitch.Broadcast"
 
-        Client.configureShared(.init(apiKey: "74e5enp33qj2", logOptions: .info))
+        Client.configureShared(.init(apiKey: "74e5enp34e52", logOptions: .info))
         Client.shared.set(user: .init(id: generateUserId()), token: .development)
         
         return true
